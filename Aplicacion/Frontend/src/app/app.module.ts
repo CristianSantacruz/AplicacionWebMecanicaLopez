f56602dd191efa9c9c6f5d25899d852d0f00d936
CristianSantacruz <santacruz-guarquila@hotmail.com>
2017-07-05 00:55:45 -0500

    Login

--- Aplicacion/Frontend/src/app/app.module.ts
+++ Aplicacion/Frontend/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import {MasterURlService} from "./services/master-url.service";
 import { HomeComponent } from './home/home.component';
 import {routing} from "./app.routes";
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- Aplicacion/Frontend/src/app/app.routes.ts
+++ Aplicacion/Frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import {Routes, RouterModule} from '@angular/router';
 import { ModuleWithProviders }  from '@angular/core';
 import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./login/login.component";
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'login', component: LoginComponent}
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
